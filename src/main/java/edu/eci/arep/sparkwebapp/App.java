5908eafdac0b4945c8be16fd349d7062b15a4fdd
JohanS11 <johan.arias@mail.escuelaing.edu.co>
2020-08-16 22:24:51 -0500

    First Commit

---
+++ src/main/java/edu/eci/arep/sparkwebapp/App.java
@@ -0,0 +1,13 @@
+package edu.eci.arep.sparkwebapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
